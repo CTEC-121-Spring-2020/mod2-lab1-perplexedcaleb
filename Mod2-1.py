8d2d87563b259bd2c188c101d142f1bb4b3d6c27
perplexedcaleb <c.howard14@students.clark.edu>
2020-04-29 12:17:30 -0700

    Completed Code

---
+++ Mod2-1.py
@@ -0,0 +1,50 @@
+"""
+CTEC 121
+Caleb Howard
+Module 2 Lab 1
+<assignment/lab description
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    """
+    A = 2
+    print("\n", A, "\n")
+    a = 22
+    print(a)
+    print()
+    AAA = 2
+    print(AAA)
+    spam = 222
+    print()
+    print(spam)
+    #print(Spam)
+    print()
+    # expression
+    #print(2)
+    print(3 - 1)
+    print((3 - 1) / 2)
+    print(5/3)
+    print(5//3)
+    print(5%3)
+    print(2 + ((3 - 1) / 2))
+    print()
+    print("The answer is:", 3 + 4, sep="-", end="\t")
+    print("end")
+    print()
+    for i in range(5):
+        print(i, end="\t")
+    print()
+    """
+    # exercise
+    classCode = "CTEC 121"
+    classCount = 18
+    className = "Introduction to Programming and Problem Solving"
+    # Welcome to <class code>. The student count is <class count> and the name of the class is <class name>
+    print("Welcome to ", classCode, ". The student count is ", classCount," and the name of the class is ", className, ".", sep="")
+main()
